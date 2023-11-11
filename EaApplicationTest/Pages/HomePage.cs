e9be5151eeded8e81fd65031f8cbbc60a8338352
Tyler Garcia <tylergarg@hotmail.com>
2023-11-11 17:39:18 -0500

    Implementation PO part 1

---
+++ EaApplicationTest/Pages/HomePage.cs
@@ -0,0 +1,27 @@
+﻿using EaFramework.Driver;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EaApplicationTest.Pages
+{
+    internal class HomePage
+    {
+        private readonly IDriverFixture _driverFixture;
+
+        public HomePage(IDriverFixture driverFixture)
+        {
+            _driverFixture = driverFixture;
+        }
+
+        private IWebElement linkHome => _driverFixture.Driver.FindElement(By.LinkText("Home"));
+        private IWebElement linkPrivacy => _driverFixture.Driver.FindElement(By.LinkText("Privacy"));
+        private IWebElement linkProduct => _driverFixture.Driver.FindElement(By.LinkText("Product"));
+
+        public void ClickProduct() => linkProduct.Click();
+
+    }
+}
---
+++ EaApplicationTest/Pages/ProductPage.cs
@@ -0,0 +1,38 @@
+﻿using EaFramework.Driver;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EaApplicationTest.Pages
+{
+    public class ProductPage
+    {
+        private readonly IDriverFixture _driverFixture;
+        public ProductPage(IDriverFixture driverFixture)
+        {
+            _driverFixture = driverFixture;
+        }
+
+        private IWebElement txtName => _driverFixture.Driver.FindElement(By.Name("Names"));
+        private IWebElement txtDescription => _driverFixture.Driver.FindElement(By.Id("Description"));
+        private IWebElement txtPrice => _driverFixture.Driver.FindElement(By.Id("Price"));
+        private IWebElement ddlProductType => _driverFixture.Driver.FindElement(By.Id("ProductType"));
+        private IWebElement lnkCreate => _driverFixture.Driver.FindElement(By.LinkText("Create"));
+        private IWebElement btnCreate => _driverFixture.Driver.FindElement(By.Id("Create"));
+
+        public void ClickCreateButton() => lnkCreate.Click();
+
+        public void CreateProduct(String name, String description, String price, String productType)
+        {
+            txtName.SendKeys(name);
+            txtDescription.SendKeys(description);
+            txtPrice.SendKeys(price);
+            ddlProductType.SendKeys(productType);
+            btnCreate.Click();
+        }
+
+    }
+}
